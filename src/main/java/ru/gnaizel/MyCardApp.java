a0c371c535196c5cd261cb522260c0696432f72a
Gnaiz <Gnaizel@mail.ru>
2025-08-18 12:00:03 +0300

    create skeleton minimal html, css

---
+++ src/main/java/ru/gnaizel/MyCardApp.java
@@ -0,0 +1,11 @@
+package ru.gnaizel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCardApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MyCardApp.class, args);
+    }
+}
